cfdce7ae7003e2e244e5d1660e5040ebccde25b9
Ricardo Dias <ricardo.a.r.dias@gmail.com>
2022-03-21 11:17:57 +0000

    Added `.xcframework` to library + package manifest

---
+++ Sources/DTxTherapeuticAdherence.xcframework/ios-arm64/DTxTherapeuticAdherence.framework/Headers/DTxTherapeuticAdherence.h
@@ -0,0 +1,21 @@
+//
+//  DTxTherapeuticAdherence
+//  Copyright © 2022 Capgemini Engineering. All rights reserved.
+//
+//  DTxTherapeuticAdherence is part of DTx SaMD Framework.
+//  It provides medication management functionalities to any SaMD
+//  mobile product that relies on DTx SaMD framework architecture.
+//
+//
+//  DTxTherapeuticAdherence.h
+//  Created by Ricardo Dias on 17/03/2022.
+//
+
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DTxTherapeuticAdherence.
+FOUNDATION_EXPORT double DTxTherapeuticAdherenceVersionNumber;
+
+//! Project version string for DTxTherapeuticAdherence.
+FOUNDATION_EXPORT const unsigned char DTxTherapeuticAdherenceVersionString[];
